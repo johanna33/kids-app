6d044f78469b259ef70a792b5312859849399139
Johanna Calderon <joha_17@hotmail.com>
2025-03-28 13:08:28 -0500

    add library for unit test report

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,20 @@
-import type {Config} from 'jest';
+import type { Config } from "jest";
 
 const config: Config = {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    verbose: true,
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  verbose: true,
+  reporters: [
+    "default",
+    [
+      "./node_modules/jest-html-reporter",
+      {
+        pageTitle: "Unit Test Report",
+        includeFailureMsg: true,
+        includeConsoleLog: true,
+      },
+    ],
+  ],
 };
 
 export default config;
